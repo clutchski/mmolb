51b4054ed73c65cab9e0111172de0397bb87a91b
clutchski <clutchski@gmail.com>
2011-12-23 11:18:01 -0500

    Drawing light.

--- public/javascripts/lights.js
+++ public/javascripts/lights.js
@@ -13,6 +13,9 @@ $(function () {
         if (console) console.log(message);
     };
 
+    //
+    // Models
+    //
 
     var Palette = Backbone.Model.extend({
 
@@ -35,6 +38,57 @@ $(function () {
 
     });
 
+
+    var LightBright = Backbone.Model.extend({
+
+        initialize : function () {
+            this.matrix = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ];
+        }
+
+    });
+
+
+    //
+    // Views
+    //
+
+
+    var LightBrightView = Backbone.View.extend({
+
+        initialize : function (options) {
+            this.palette = options.palette;
+            this.model.bind('change', _.bind(this.update, this));
+
+            this.canvas = document.getElementById('lights');
+            this.context = this.canvas.getContext('2d');
+            this.update();
+        },
+
+        update : function () {
+            var x = 100;
+            var y = 100;
+            _.each(this.model.matrix, function (row, i) {
+                _.each(row, function (cell, j) {
+                    this.drawLight(x + i * 50, y + j * 50, '#fff');
+                }, this);
+            }, this);
+        },
+
+        drawLight : function (x, y, color) {
+            log("p " + x + " " +  y);
+            this.context.fillStyle = color;
+            this.context.beginPath();
+            this.context.arc(x, y, 20, 0, Math.PI * 2);
+            this.context.fill()
+        }
+
+    });
+
+
     var PaletteView = Backbone.View.extend({
 
         initialize : function () {
@@ -54,20 +108,26 @@ $(function () {
         },
 
         onModelChange : function () {
-            var color = this.model.getColor();
             this.paletteColors
                     .removeClass('selected')
-                    .filter('[color="' + color + '"]')
+                    .filter('[color="' + this.model.getColor() + '"]')
                     .addClass('selected');
         }
 
     });
 
 
+    //
+    // Run the application.
+    //
     var palette = new Palette();
     var paletteView = new PaletteView({el:$('#palette'), model:palette});
     paletteView.bind('color_selected', function (color) {
         palette.setColor(color);
     });
+
+    var lightBright = new LightBright();
+    var lightBrightView = new LightBrightView({id:'lights',
+            model:lightBright, palette:palette});
 
 });

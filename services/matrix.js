9aa2a9036b34dd64c145fea48eb1f5678e9dca4e
clutchski <clutchski@gmail.com>
2012-01-15 17:56:45 -0500

    Adding an async native mongo version.

--- services/matrix.js
+++ services/matrix.js
@@ -2,39 +2,66 @@
 // The app's persistence layer.
 //
 
-
-var Mongolian = require('mongolian');
-
-
-// DB config. FIXME: this should live somewhere nicer.
-var url = process.env.MONGOHQ_URL || 'mongodb://localhost/lumiere';
-
-console.log("connection to db " + url);
-
-// Initialize our db and collection.
-var db = new Mongolian(url);
-var elements = db.collection('elements');
+var mongo = require('mongodb');
 
 
-// Return the entire matrix.
-exports.getMatrix = function (callback) {
-    elements.find().toArray(function (err, array) {
+// The app's db url.
+var URI = process.env.MONGOLAB_URI || 'mongodb://localhost/lumiere';
+
+
+// Return a connection to the mongo database via the callback.
+var connect = function (callback) {
+    mongo.connect(URI, {}, function (err, db) {
         if (err) {
             return callback(err);
         }
-        var matrix = [];
-        array.forEach(function (e) {
-            matrix[e.i] = matrix[e.j] || [];
-            matrix[e.i][e.j] = e.color;
+
+        // Set up error handling.
+        db.addListener("error", function (error) {
+            console.log("Error connecting to MongoLab:\n" + error);
         });
-        return callback(null, matrix);
+
+        return callback(null, db);
+    });
+};
+
+// Return a reference to a collection.
+var collection = function (name, callback) {
+    connect(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        return db.collection(name, callback);
+    });
+};
+
+// Return the entire matrix.
+exports.getMatrix = function (callback) {
+    collection('elements', function (err, elements) {
+        if (err) {
+            return callback(err);
+        }
+        elements.find().toArray(function (e, array) {
+            if (e) {
+                return callback(e);
+            }
+            var matrix = [];
+            array.forEach(function (e) {
+                matrix[e.i] = matrix[e.j] || [];
+                matrix[e.i][e.j] = e.color;
+            });
+            return callback(null, matrix);
+        });
     });
 };
 
 // Update one element of the matrix to the given color.
 exports.setElement = function (element, callback) {
-    var query = {i: element.i, j: element.j};
-    elements.update(query, element, true, function (err) {
-        return callback(err);
+    collection('elements', function (err, elements) {
+        if (err) {
+            return callback(err);
+        }
+        var query = {i: element.i, j: element.j};
+        elements.update(query, element, {upsert: true}, callback);
     });
 };
